12ec0db85818b8cfa280ea26bf59b5a0aefc5c19
Aditya Dubey <adityadubeym98@gmail.com>
2021-10-12 13:00:26 +0530

    removed sensitive info

--- rentmate_sys/settings.py
+++ rentmate_sys/settings.py
@@ -128,8 +128,8 @@ STATIC_PATH,
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "help.rentmate@gmail.com"
-EMAIL_HOST_PASSWORD = "adityajay"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
 
 #for media files
